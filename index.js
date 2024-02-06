75e53c0dee4fb0447c9f88986ca63fcd4dddb6f7
João Pantaleão <jopantaleao@gmail.com>
2024-02-06 22:42:52 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+function fibonacciGenerator (n) {
+
+    var output = [];
+    if (n == 1){
+        output.push(0);
+    }
+    else if(n == 2){
+        output.push(0);
+        output.push(1);
+    }
+    else {
+        output.push(0);
+        output.push(1);
+        while(n > output.length){
+            output.push(output[output.length-2] + output[output.length-1]);
+          
+        }
+
+    }
+    console.log(output)
+
+}
+fibonacciGenerator(10);
